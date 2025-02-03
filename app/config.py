164f303125c9080ab7182cee97a604e5c2cc7d11
AB-DEV28 <88678598+AB-DEV28@users.noreply.github.com>
2025-02-03 19:31:17 +0100

    Update configuration and Docker setup

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 import os
-import socket
+import asyncio
 from motor.motor_asyncio import AsyncIOMotorClient as MotorAsyncIOMotorClient
 from dotenv import load_dotenv
 import dns.resolver
@@ -11,31 +11,19 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")  # Default if not found
 ALGORITHM = os.getenv("ALGORITHM", "HS256")  # Default to HS256 if not set
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))  # Default to 30 minutes if not set
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
 
 # Set DNS resolver to use Google DNS
 dns.resolver.default_resolver = dns.resolver.Resolver(configure=False)
 dns.resolver.default_resolver.nameservers = ['8.8.8.8', '8.8.4.4']
 
-def test_dns_resolution(hostname):
-    try:
-        socket.gethostbyname(hostname)
-        print(f"DNS resolution for {hostname} succeeded.")
-    except socket.error as err:
-        print(f"DNS resolution for {hostname} failed: {err}")
-
 def init_db_client():
-    db_uri = os.getenv('MONGODB_URI')
-    if not db_uri:
-        raise ValueError("MONGODB_URI environment variable not set")
-    
-    # Extract hostname from URI and test DNS resolution
-    hostname = db_uri.split('@')[1].split('/')[0]
-    test_dns_resolution(hostname)
-    
+    db_uri = f"mongodb+srv://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}?retryWrites=true&w=majority"
     return MotorAsyncIOMotorClient(
         db_uri,
+        io_loop=asyncio.get_running_loop(),
         uuidRepresentation="standard",
-        serverSelectionTimeoutMS=10000  # Increase timeout to 10 seconds
     )
-
-# ...existing code...
